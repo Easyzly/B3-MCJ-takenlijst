9e9222632c4f0bdac443dd337ff54d27c02aabb9
Easyzly <117265805+Easyzly@users.noreply.github.com>
2023-04-06 16:14:21 +0200

    +1.5

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
 <a href="<?php echo $base_url; ?>/index.php">Home</a>
 <a href="<?php echo $base_url; ?>/task/index.php">task home</a>
 <a href="<?php echo $base_url; ?>/task/create.php">task create</a>
+<a href="<?php echo $base_url; ?>/task/done.php">task done</a>
---
+++ task/done.php
@@ -0,0 +1,34 @@
+<head>
+    <?php require_once '../head.php'; ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require_once("../header.php"); ?>
+
+    <?php 
+        require_once("../backend/conn.php");
+        $query = "SELECT * FROM taken WHERE status = 'done'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+
+        $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <!-- Table for displaying tasks -->
+    <table>
+        <tr>
+            <th>Titel</th>
+            <th>Afdeling</th>
+            <th>Edit</th>
+        </tr>
+        <?php foreach($tasks as $task): ?>
+            <tr>
+                <td><?php echo($task['titel']) ?></td>
+                <td><?php echo($task['afdeling']) ?></td>
+                <td><a href="edit.php?id=<?php echo $task['id'] ?>">Edit</a></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
