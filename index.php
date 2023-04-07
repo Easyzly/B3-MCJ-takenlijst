48475b517169e9395cfe421086762c1c3deb66c3
MartijnCurio <124584804+MartijnCurio@users.noreply.github.com>
2023-04-07 09:42:00 +0200

    working on index.php styling
    not yet finished, but needed to get committed

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!doctype html>
 <html lang="nl">
-
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
 </head>
-
 <body>
-    <nav>
-        <?php require_once("header.php"); ?>
-    </nav>
-</body>
+    <main>
+        <div class="background">
+            <header>
+                <a href="index.html" class="heartbeatLogo">Developerland</a>
+                <?php require_once("header.php"); ?>
+                <img class="accountLogo" src="img/accountlogo.png" width="50" alt="accountLogo">
+            </header>
+            
+            <img class="devLandLogo" class="imageButtons" src="img/logo-big-v3.png" alt="devLandLogo">
+        </div>
+        <div class="imageButtons">
+            <div class="imageButton">
+                
+            </div>
+        </div>
+    </main>
+    <footer>
 
+    </footer>
+</body>
 </html>
