04e9ecc852662dd9d2ff4fef60447dbfee4f203f
Easyzly <117265805+Easyzly@users.noreply.github.com>
2023-04-11 13:13:29 +0200

    +header fix
    +redirect to userpage

--- header.php
+++ header.php
@@ -7,9 +7,8 @@
 
     <nav>
         <a href="<?php echo $base_url ?>/index.php">Home</a>
-        <a href="<?php echo $base_url ?>/info.php">Info</a>
         <a href="<?php echo $base_url ?>/task/index.php">Taken</a>
     </nav>
 
-    <a href="<?php $base_url; ?>account_files/userPage.php"><img src="<?php echo $base_url; ?>/img/accountlogo.png" width="50" alt="account logo"></a>
+    <a href="<?php $base_url; ?>userPage.php"><img src="<?php echo $base_url; ?>/img/accountlogo.png" width="50" alt="account logo"></a>
 </div>
