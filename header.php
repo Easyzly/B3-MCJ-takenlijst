dd78b71d15c4bffe1013a741e2cf0f1b791ad51e
Easyzly <117265805+Easyzly@users.noreply.github.com>
2023-04-11 12:19:45 +0200

    +header update

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <div class="headerDiv">
     <a href="../index.html" class="HeartbeatLogo">Developerland</a>
     <?php 
-        $base_url = 'http://localhost/takenlijst';
+        $base_url = 'http://localhost/B3-MCJ-takenlijst';
     ?>
 
     <nav>
         <a href="<?php echo $base_url ?>/index.php">Home</a>
         <a href="<?php echo $base_url ?>/info.php">Info</a>
-        <a href="<?php echo $base_url ?>/taskPage.php">Taken</a>
+        <a href="<?php echo $base_url ?>/task/index.php">Taken</a>
     </nav>
 
     <a href="<?php $base_url; ?>account_files/userPage.php"><img src="<?php echo $base_url; ?>/img/accountlogo.png" width="50" alt="account logo"></a>
