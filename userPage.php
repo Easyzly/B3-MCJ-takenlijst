f2fa8bed6649ce0088729a016ec3c7113142f522
MartijnCurio <124584804+MartijnCurio@users.noreply.github.com>
2023-04-17 11:44:50 +0200

    Fixed styling voor userpage

--- userPage.php
+++ userPage.php
@@ -26,94 +26,105 @@ if (isset($_SESSION['id'])) {
 ?>
 
 <body>
-    <header>
-        <?php require_once('header.php') ?>
-    </header>
+    <div class="background">
+        <div class="background-gradient">
+            <header>
+                <?php require_once('header.php') ?>
+            </header>
 
-    <!-- User settings and details -->
-    <form action="backend/accountController.php" method="post">
-        <input type="hidden" name="action" value="Logout">
-        <input type="submit" value="logout">
-    </form>
+            <div class="userpage-container">
+                <p>Selecteer een afdeling:</p><br>
 
-    <!-- Afdeling selector  -->
-    <div class="department-buttons">
-        <a href="userPage.php?afdeling=Horeca">Horeca</a>
-        <a href="userPage.php?afdeling=Personeel">Personeel</a>
-        <a href="userPage.php?afdeling=Techniek">Techniek</a>
-        <a href="userPage.php?afdeling=Inkoop">Inkoop</a>
-        <a href="userPage.php?afdeling=Klantenservice">Klantenservice</a>
-        <a href="userPage.php?afdeling=Groen">Groen</a>
-        <a href="userPage.php?afdeling=noFilter">Alle</a>
-        <a href="userPage.php?afdeling=User">Persoonlijke taken</a>
+                <!-- Afdeling selector  -->
+                <div class="department-buttons">
+                    <a href="userPage.php?afdeling=Horeca">Horeca</a>
+                    <a href="userPage.php?afdeling=Personeel">Personeel</a>
+                    <a href="userPage.php?afdeling=Techniek">Techniek</a>
+                    <a href="userPage.php?afdeling=Inkoop">Inkoop</a>
+                    <a href="userPage.php?afdeling=Klantenservice">Klantenservice</a>
+                    <a href="userPage.php?afdeling=Groen">Groen</a>
+                    <a href="userPage.php?afdeling=noFilter">Alle</a>
+                    <a href="userPage.php?afdeling=User">Persoonlijke taken</a>
+                </div><br>
+
+                <!-- Afdeling checker -->
+                <?php
+                if ($_GET['afdeling'] == null or !isset($_GET['afdeling'])) {
+                    header("Location: userPage.php?afdeling=Horeca");
+                } elseif ($_GET['afdeling'] == "noFilter") {
+                    require_once("backend/conn.php");
+                    $query = "SELECT * FROM taken ORDER BY deadline";
+                    $statement = $conn->prepare($query);
+                    $statement->execute();
+
+                    $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+                } elseif ($_GET['afdeling'] == "User") {
+                    require_once("backend/conn.php");
+                    $query = "SELECT * FROM taken WHERE id = :id ORDER BY deadline";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([":id" => $userAccount['id']]);
+
+                    $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    $afdeling = $_GET['afdeling'];
+
+                    require_once("backend/conn.php");
+                    $query = "SELECT * FROM taken WHERE afdeling = :afdeling ORDER BY deadline";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([":afdeling" => $afdeling]);
+
+                    $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+                }
+                ?>
+
+                <!-- Table for displaying tasks -->
+                <?php if (count($tasks) > 0): ?>
+                    <table>
+                        <tr>
+                            <th>Titel</th>
+                            <th>Afdeling</th>
+                            <th>Status</th>
+                            <th>deadline</th>
+                            <th>Gebruiker</th>
+                            <th>Edit</th>
+                        </tr>
+                        <?php foreach ($tasks as $task): ?>
+                            <tr>
+                                <td>
+                                    <?php echo ($task['titel']) ?>
+                                </td>
+                                <td>
+                                    <?php echo ($task['afdeling']) ?>
+                                </td>
+                                <td>
+                                    <?php echo ($task['status']) ?>
+                                </td>
+                                <td>
+                                    <?php echo ($task['deadline']) ?>
+                                </td>
+                                <td>
+                                    <?php echo ($task['user']) ?>
+                                </td>
+                                <td><a href="task/edit.php?id=<?php echo $task['id'] ?>">Edit</a></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </table>
+                <?php else: ?>
+                    <p>Je hebt geen taken voor deze afdeling.</p>
+                <?php endif ?>
+
+                <!-- User settings and details -->
+                <form action="backend/accountController.php" method="post">
+                    <input type="hidden" name="action" value="Logout">
+                    <input type="submit" value="logout">
+                </form>
+            </div>
+
+            <footer>
+                <?php require_once('footer.php') ?>
+            </footer>
+        </div>
     </div>
-
-    <!-- Afdeling checker -->
-    <?php
-    if ($_GET['afdeling'] == null or !isset($_GET['afdeling'])) {
-        header("Location: userPage.php?afdeling=Horeca");
-    } elseif ($_GET['afdeling'] == "noFilter") {
-        require_once("backend/conn.php");
-        $query = "SELECT * FROM taken ORDER BY deadline";
-        $statement = $conn->prepare($query);
-        $statement->execute();
-
-        $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
-    } elseif ($_GET['afdeling'] == "User") {
-        require_once("backend/conn.php");
-        $query = "SELECT * FROM taken WHERE id = :id ORDER BY deadline";
-        $statement = $conn->prepare($query);
-        $statement->execute([":id" => $userAccount['id']]);
-
-        $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
-    } else {
-        $afdeling = $_GET['afdeling'];
-
-        require_once("backend/conn.php");
-        $query = "SELECT * FROM taken WHERE afdeling = :afdeling ORDER BY deadline";
-        $statement = $conn->prepare($query);
-        $statement->execute([":afdeling" => $afdeling]);
-
-        $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-    ?>
-    </div>
-
-    <!-- Table for displaying tasks -->
-    <table>
-        <tr>
-            <th>Titel</th>
-            <th>Afdeling</th>
-            <th>Status</th>
-            <th>deadline</th>
-            <th>Gebruiker</th>
-            <th>Edit</th>
-        </tr>
-        <?php foreach ($tasks as $task): ?>
-            <tr>
-                <td>
-                    <?php echo ($task['titel']) ?>
-                </td>
-                <td>
-                    <?php echo ($task['afdeling']) ?>
-                </td>
-                <td>
-                    <?php echo ($task['status']) ?>
-                </td>
-                <td>
-                    <?php echo ($task['deadline']) ?>
-                </td>
-                <td>
-                    <?php echo ($task['user']) ?>
-                </td>
-                <td><a href="task/edit.php?id=<?php echo $task['id'] ?>">Edit</a></td>
-            </tr>
-        <?php endforeach ?>
-    </table>
-
-    <footer>
-        <?php require_once('footer.php') ?>
-    </footer>
 </body>
 
 </html>
