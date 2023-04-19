069771ca5f61baa3741854fee6d6e21aa0b4ea0c
JustNoob2005 <corne.jobse2005@gmail.com>
2023-04-19 10:05:52 +0200

    add logo

--- head.php
+++ head.php
@@ -4,4 +4,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
-<link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo $base_url; ?>logo-big-v3.ico" type="image/x-icon">
