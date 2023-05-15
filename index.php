13519a642b7b05fc8df0f96f4f1da4726defb67a
Easyzly <117265805+Easyzly@users.noreply.github.com>
2023-05-15 11:01:44 +0200

    +Footer dow  & header up

--- index.php
+++ index.php
@@ -20,39 +20,38 @@
     ?>
 </head>
 
+
+
 <body>
+    <header>
+        <?php require_once("header.php"); ?>
+    </header>
+
+
     <main>
         <div class="background">
-            <div class="background-gradient">
-                <header>
-                    <?php require_once("header.php"); ?>
-                </header>
-                <img class="logo" src="img/logo-big-v3.png" alt="devLandLogo">
-                <!-- Widget group -->
-                <div class="WidgetBlock">
-                    <h2>Aankondigingen</h2>
-                    <p>"Maak je leven gemakkelijker met onze nieuwe takenlijst website! Of je nu op zoek bent naar een
-                        betere manier om je dagelijkse taken te beheren of om je werk beter te organiseren, onze
-                        takenlijst website biedt de oplossing die je nodig hebt.</p>
-                </div>
-                <br>
-                <div class="WidgetBlock">
-                    <h2>Over ons</h2>
-                    <p>Developerland is opgericht door een team van gepassioneerde technologieliefhebbers met als doel
-                        een uniek pretpark te creëren dat gericht is op het verkennen en ervaren van de nieuwste
-                        technologische innovaties.</p>
-                    <br>
-                    <p>Onze medewerkers zijn allemaal experts op hun gebied en zijn gepassioneerd over het delen van hun
-                        kennis en enthousiasme met anderen. We bieden educatieve workshops en presentaties aan om
-                        bezoekers te informeren over de nieuwste trends en ontwikkelingen op het gebied van technologie,
-                        zodat ze geïnspireerd kunnen worden om hun eigen creativiteit en innovatie te verkennen.</p>
-                </div>
-                <br>
-                <div class="WidgetBlock imgtype">
-                    <h2>Het Team</h2>
-                </div>
-                <br>
+            <img class="logo" src="img/logo-big-v3.png" alt="devLandLogo">
+            <!-- Widget group -->
+            <div class="WidgetBlock">
+                <h2>Aankondigingen</h2>
+                <p>"Maak je leven gemakkelijker met onze nieuwe takenlijst website! Of je nu op zoek bent naar een
+                    betere manier om je dagelijkse taken te beheren of om je werk beter te organiseren, onze
+                    takenlijst website biedt de oplossing die je nodig hebt.</p>
             </div>
+            <div class="WidgetBlock">
+                <h2>Over ons</h2>
+                <p>Developerland is opgericht door een team van gepassioneerde technologieliefhebbers met als doel
+                    een uniek pretpark te creëren dat gericht is op het verkennen en ervaren van de nieuwste
+                    technologische innovaties.</p>
+                <p>Onze medewerkers zijn allemaal experts op hun gebied en zijn gepassioneerd over het delen van hun
+                    kennis en enthousiasme met anderen. We bieden educatieve workshops en presentaties aan om
+                    bezoekers te informeren over de nieuwste trends en ontwikkelingen op het gebied van technologie,
+                    zodat ze geïnspireerd kunnen worden om hun eigen creativiteit en innovatie te verkennen.</p>
+            </div>
+            <div class="WidgetBlock imgtype">
+                <h2>Het Team</h2>
+            </div>
+            <br>
         </div>
     </main>
 
